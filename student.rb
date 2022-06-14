d0ee19dde3b988b1d7decd5e1e172d7a3db70376
raoakif <61361037+RaoAkif@users.noreply.github.com>
2022-06-14 05:51:50 +0500

    add classes person, student and teacher

---
+++ student.rb
@@ -0,0 +1,10 @@
+class Student < Person
+  def initialize(name = "Unknown", classroom, age, parental_permission: true)
+    super(name, age, parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
+end
